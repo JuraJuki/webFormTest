5543271ab20af4bdf646c67c6405365afcb01254
JuraJuki <juraypavlovic@gmail.com>
2023-03-01 08:51:04 +0100

    deploy newest

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('test_app') as HTMLElement
-);
-root.render(
+const el = document.getElementById("test_app");
+console.log(el);
+
+ReactDOM.createRoot(el as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
